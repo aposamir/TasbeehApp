c5e97fa81695dd3280ee3002fd6ade60ee28484b
aposamir <apos14235@gmail.com>
2026-09-10 10:36:04 +0300

    Refactor overlay permission handling in MainActivity

--- app/src/main/java/com/aposamir/tasbeehapp/MainActivity.java
+++ app/src/main/java/com/aposamir/tasbeehapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -41,27 +40,11 @@ public class MainActivity extends AppCompatActivity {
         } else {
             registerReceiver(bubbleReceiver, new IntentFilter("BUBBLE_TAPPED"));
         }
-
-        checkOverlayPermission();
-    }
-
-    private void checkOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, OVERLAY_PERMISSION_REQ);
-        } else {
-            startService(new Intent(this, FloatingService.class));
-        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == OVERLAY_PERMISSION_REQ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-                startService(new Intent(this, FloatingService.class));
-            }
-        }
     }
 
     @Override
@@ -81,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     public class WebAppInterface {
+
         @JavascriptInterface
         public void updateCount(int count) {
             Intent intent = new Intent("WEB_UPDATED");
@@ -89,20 +73,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @JavascriptInterface
-        public void toggleBubble() {
-            SharedPreferences prefs = getSharedPreferences("bubble_prefs", MODE_PRIVATE);
-            boolean isRunning = prefs.getBoolean("bubble_running", true);
-            if (isRunning) {
-                stopService(new Intent(MainActivity.this, FloatingService.class));
-                prefs.edit().putBoolean("bubble_running", false).apply();
-            } else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(MainActivity.this)) {
-                    checkOverlayPermission();
-                } else {
-                    startService(new Intent(MainActivity.this, FloatingService.class));
-                }
-                prefs.edit().putBoolean("bubble_running", true).apply();
+        public boolean hasOverlayPermission() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return Settings.canDrawOverlays(MainActivity.this);
             }
+            return true;
+        }
+
+        @JavascriptInterface
+        public void requestOverlayPermission() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(MainActivity.this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ);
+            }
+        }
+
+        @JavascriptInterface
+        public void showOverlayBubble(double scale) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(MainActivity.this)) {
+                return;
+            }
+            Intent intent = new Intent(MainActivity.this, FloatingService.class);
+            intent.putExtra("scale", scale);
+            startService(intent);
+        }
+
+        @JavascriptInterface
+        public void hideBubble() {
+            stopService(new Intent(MainActivity.this, FloatingService.class));
         }
     }
 
