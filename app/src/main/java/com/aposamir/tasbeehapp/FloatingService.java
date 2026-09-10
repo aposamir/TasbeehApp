6662b77d7dfbf431173f6a710724223c0c97d2b4
aposamir <apos14235@gmail.com>
2026-09-10 13:32:58 +0300

    Update DEFAULT_SCALE value in FloatingService

--- app/src/main/java/com/aposamir/tasbeehapp/FloatingService.java
+++ app/src/main/java/com/aposamir/tasbeehapp/FloatingService.java
@@ -18,7 +18,7 @@ import android.widget.TextView;
 
 public class FloatingService extends Service {
 
-    private static final double DEFAULT_SCALE = 4.0 / 9.0; // احتياطي فقط؛ القيمة الفعلية تأتي من JS عبر NATIVE_BUBBLE_SCALE
+    private static final double DEFAULT_SCALE = 8.0 / 9.0; // احتياطي فقط؛ القيمة الفعلية تأتي من JS عبر NATIVE_BUBBLE_SCALE
     private static final int BASE_BUBBLE_DP = 128; // يطابق layout_width/height الثابت في layout_floating_bubble.xml
 
     private WindowManager windowManager;
@@ -177,4 +177,3 @@ public class FloatingService extends Service {
         }
     }
 }
- 
